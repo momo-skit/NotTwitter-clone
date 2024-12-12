eb0729ab20c4283139a9b2a6344941be93e08187
Pandacake <omnhuheolatui@gmail.com>
2024-12-13 06:46:17 +0700

    4- prep app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+
 import authRoutes from "./routes/auth.route.js"; // authRoutes is essentially the router object that was exported from auth.routes.js, just Direct default import with a custom name:
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -19,6 +21,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body.. dont add to muc hlimit cuz DOS attacj-- theyu can bomb u with heavy requesr
 app.use(express.urlencoded({ extended: true })); // parse form data (urlencoded)
@@ -29,6 +32,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
