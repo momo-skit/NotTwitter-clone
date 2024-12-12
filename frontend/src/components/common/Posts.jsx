2ac4f2e607fe408134edcca2828a6eff813888d2
Pandacake <omnhuheolatui@gmail.com>
2024-12-13 02:09:36 +0700

    3- profile page posts and users info fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,17 +3,17 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
   const getPostEndPoint = () => {
     switch (feedType) {
       case "forYou":
         return "/api/posts/all";
       case "following":
         return "/api/posts/following";
-      // case "posts":
-      // 	return `/api/posts/user/${username}`;
-      // case "likes":
-      // 	return `/api/posts/likes/${userId}`;
+      case "posts":
+        return `/api/posts/user/${username}`; // this username passed as prop from profile page(parent)
+      case "likes":
+        return `/api/posts/likes/${userId}`;
       default:
         return "/api/posts/all";
     }
@@ -46,7 +46,7 @@ const Posts = ({ feedType }) => {
 
   useEffect(() => {
     refetch();
-  }, [feedType, refetch]); //username
+  }, [feedType, refetch, username]);
 
   return (
     <>
